1964eee6cce92c7fb151de896e3a99f0584c6154
Matteo Nini <m.nini@unimore.it>
2025-06-19 12:18:04 +0000

    Added pick_block()

--- include/StateMachine.hpp
+++ include/StateMachine.hpp
@@ -5,6 +5,12 @@
 #include <shape_msgs/msg/solid_primitive.hpp>
 #include "RobotTaskStatus.hpp"
 
+#include "Motionplanning_arms.hpp"
+
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "control_msgs/action/follow_joint_trajectory.hpp"
+#include "trajectory_msgs/msg/joint_trajectory_point.hpp"
+
 enum class RobotState {
     IDLE,
     ADD_OBSTACLE,
@@ -27,8 +33,13 @@ private:
     int block_count_;
     int layer_count_;
 
-    // Timers
-    rclcpp::TimerBase::SharedPtr timer_;
+    // Publishers
+    rclcpp::Publisher<moveit_msgs::msg::PlanningScene>::SharedPtr planning_scene_publisher_;
+
+    std::shared_ptr<icr_Motionplanning_arms> motion_node_;
+
+    // Define Pick Pose
+    geometry_msgs::msg::Pose pick_pose_;
 
     void pickBlock();
     void addObstacle(geometry_msgs::msg::PoseStamped pose, shape_msgs::msg::SolidPrimitive dimension);
--- src/StateMachine.cpp
+++ src/StateMachine.cpp
@@ -1,9 +1,24 @@
 #include "StateMachine.hpp"
 
 StateMachine::StateMachine(rclcpp::Node::SharedPtr node)
-: node_(node), state_(RobotState::IDLE), block_count_(0), layer_count_(0) {
+: node_(node), state_(RobotState::IDLE), block_count_(0), layer_count_(0) { 
 
-    RCLCPP_INFO(node_->get_logger(),"Hello2");
+    // Create a shared pointer to your node
+    motion_node_ = std::make_shared<icr_Motionplanning_arms>();
+
+    // Publisher
+    // planning_scene_publisher_ = motion_node->create_publisher<moveit_msgs::msg::PlanningScene>(
+    // "/planning_scene", rclcpp::QoS(1));
+
+    pick_pose_.position.x = 0.4;
+    pick_pose_.position.y = 0.0;
+    pick_pose_.position.z = 0.5;
+    // Simple orientation (quaternion), facing forward
+    pick_pose_.orientation.x = 0.0;
+    pick_pose_.orientation.y = 0.707;
+    pick_pose_.orientation.z = 0.0;
+    pick_pose_.orientation.w = 0.707;
+
 
 }
 
@@ -50,10 +65,22 @@ void StateMachine::update() {
 
 void StateMachine::pickBlock() {
     
+    try {
+        motion_node_->motion_planning_control(pick_pose_, RobotTaskStatus::Arm::ARM_torso);
+        RCLCPP_INFO(node_->get_logger(), "Arm moved to pick position.");
+    } catch (const std::exception &e) {
+        RCLCPP_ERROR(node_->get_logger(), "Failed to move to pick position: %s", e.what());
+    }
+
+    motion_node_->GripperControl("CLOSE");
+
 }
 
 void StateMachine::moveToTable(int block_index, int layer_index) {
     
+    // Publish goal trajectory
+    
+
 }
 
 void StateMachine::placeBlock() {
