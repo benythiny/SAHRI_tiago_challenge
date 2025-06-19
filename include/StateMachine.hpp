1f571b3c9b7d93a5d505190244b61b1671dcfc86
Iordanis Antypas <jordanadipas@gmail.com>
2025-06-19 11:18:07 +0000

    adds bricks but coordinates are fucked up --> TODO maybe find table URDF

--- include/StateMachine.hpp
+++ include/StateMachine.hpp
@@ -3,9 +3,12 @@
 #include "rclcpp/rclcpp.hpp"
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <shape_msgs/msg/solid_primitive.hpp>
+#include <moveit_msgs/msg/planning_scene.hpp>
+#include <moveit_msgs/msg/collision_object.hpp>
 #include "RobotTaskStatus.hpp"
 
-enum class RobotState {
+enum class RobotState
+{
     IDLE,
     ADD_OBSTACLE,
     PICK_BLOCK,
@@ -15,23 +18,36 @@ enum class RobotState {
     FINISHED
 };
 
-class StateMachine {
+class StateMachine
+{
 public:
     StateMachine(rclcpp::Node::SharedPtr node);
-
     void update();
 
 private:
+    // params
+    static constexpr double TABLE_X = 1.0; 
+    static constexpr double TABLE_Y = 0.5; 
+    static constexpr double TABLE_Z = 0.5;
+    static constexpr double TABLE_DX = 1.2; 
+    static constexpr double TABLE_DY = 1.2; 
+    static constexpr double TABLE_DZ = 0.1;
+
+    static constexpr double BLOCK_DX = 0.15;
+    static constexpr double BLOCK_DY = 0.025;
+    static constexpr double BLOCK_DZ = 0.05;
+
     rclcpp::Node::SharedPtr node_;
     RobotState state_;
     int block_count_;
     int layer_count_;
+    bool table_added_;
+    rclcpp::Publisher<moveit_msgs::msg::PlanningScene>::SharedPtr planning_scene_pub_;
 
-    // Timers
-    rclcpp::TimerBase::SharedPtr timer_;
-
+    // state actions
     void pickBlock();
-    void addObstacle(geometry_msgs::msg::PoseStamped pose, shape_msgs::msg::SolidPrimitive dimension);
+    void addObstacle(geometry_msgs::msg::PoseStamped pose,
+                     shape_msgs::msg::SolidPrimitive dimension);
     void moveToTable(int block_index, int layer_index);
     void placeBlock();
 };
--- src/StateMachine.cpp
+++ src/StateMachine.cpp
@@ -1,61 +1,110 @@
 #include "StateMachine.hpp"
 
 StateMachine::StateMachine(rclcpp::Node::SharedPtr node)
-: node_(node), state_(RobotState::IDLE), block_count_(0), layer_count_(0) {
-
-    RCLCPP_INFO(node_->get_logger(),"Hello2");
-
+    : node_(node),
+      state_(RobotState::IDLE),
+      block_count_(0),
+      layer_count_(0),
+      table_added_(false)
+{
+    planning_scene_pub_ = node_->create_publisher<moveit_msgs::msg::PlanningScene>(
+        "/monitored_planning_scene", rclcpp::QoS(1));
+    RCLCPP_INFO(node_->get_logger(), "StateMachine up");
 }
 
+void StateMachine::update()
+{
+    switch (state_)
+    {
+    case RobotState::IDLE:
+        RCLCPP_INFO(node_->get_logger(), "Starting stacking...");
+        state_ = RobotState::ADD_OBSTACLE;
+        break;
 
-void StateMachine::update() {
-    switch (state_) {
-        case RobotState::IDLE:
-            RCLCPP_INFO(node_->get_logger(), "Starting stacking...");
-            state_ = RobotState::ADD_OBSTACLE;
-            break;
+    case RobotState::ADD_OBSTACLE:
+    {
+        RCLCPP_INFO(node_->get_logger(), "Adding obstacle…");
+        geometry_msgs::msg::PoseStamped pose;
+        shape_msgs::msg::SolidPrimitive dim;
 
-        case RobotState::ADD_OBSTACLE:
-            RCLCPP_INFO(node_->get_logger(), "Adding obstacle...");
-            state_ = RobotState::PICK_BLOCK;
-            break;
+        pose.header.frame_id = "base_footprint";
+        pose.pose.position.x = TABLE_X;
+        pose.pose.position.y = TABLE_Y;
+        pose.pose.orientation.w = 1.0;
 
-        case RobotState::PICK_BLOCK:
-            RCLCPP_INFO(node_->get_logger(), "Picking block...");
-            pickBlock();
-            state_ = RobotState::MOVE_TO_TABLE;
-            break;
+        if (!table_added_)
+        {
+            // first time: add table
+            pose.pose.position.z = TABLE_Z;
+            dim.type = dim.BOX;
+            dim.dimensions = {TABLE_DX, TABLE_DY, TABLE_DZ};
+            table_added_ = true;
+        }
+        else
+        {
+            // every subsequent call: stack a Jenga block
+            double table_top_z = TABLE_Z - TABLE_DZ / 2.0;
+            double z = table_top_z + block_count_ * BLOCK_DZ // full layers below
+                       + BLOCK_DZ / 2.0;                               // half above current layer
+            pose.pose.position.z = z;
+            dim.type = dim.BOX;
+            dim.dimensions = {BLOCK_DX, BLOCK_DY, BLOCK_DZ};
+        }
 
-        case RobotState::MOVE_TO_TABLE:
-            RCLCPP_INFO(node_->get_logger(), "Moving to table...");
-            moveToTable(block_count_, layer_count_);
-            state_ = RobotState::PLACE_BLOCK;
-            break;
+        addObstacle(pose, dim);
+        state_ = RobotState::PICK_BLOCK;
+        break;
+    }
 
-        case RobotState::PLACE_BLOCK:
-            RCLCPP_INFO(node_->get_logger(), "Placing block...");
-            placeBlock();
-            block_count_++;
-            state_ = RobotState::CHECK_LAYER;
-            break;
+    case RobotState::PICK_BLOCK:
+        RCLCPP_INFO(node_->get_logger(), "Picking block…");
+        pickBlock();
+        state_ = RobotState::MOVE_TO_TABLE;
+        break;
 
-        case RobotState::CHECK_LAYER:
-            if (block_count_ % 2 == 0) {
-                layer_count_++;
-            }
-            state_ = RobotState::ADD_OBSTACLE;
-            break;
+    case RobotState::MOVE_TO_TABLE:
+        RCLCPP_INFO(node_->get_logger(), "Moving to table…");
+        moveToTable(block_count_, layer_count_);
+        state_ = RobotState::PLACE_BLOCK;
+        break;
+
+    case RobotState::PLACE_BLOCK:
+        RCLCPP_INFO(node_->get_logger(), "Placing block…");
+        placeBlock();
+        block_count_++;
+        state_ = RobotState::CHECK_LAYER;
+        break;
+
+    case RobotState::CHECK_LAYER:
+        if (block_count_ % 2 == 0)
+        {
+            layer_count_++;
+        }
+        state_ = RobotState::ADD_OBSTACLE;
+        break;
+
+    default:
+        break;
     }
 }
 
-void StateMachine::pickBlock() {
-    
+void StateMachine::addObstacle(geometry_msgs::msg::PoseStamped pose,
+                               shape_msgs::msg::SolidPrimitive dim)
+{
+    moveit_msgs::msg::CollisionObject co;
+    co.header.frame_id = pose.header.frame_id;
+    co.id = table_added_ ? "block_" + std::to_string(block_count_) : "table";
+    co.primitives.push_back(dim);
+    co.primitive_poses.push_back(pose.pose);
+    co.operation = moveit_msgs::msg::CollisionObject::ADD;
+
+    moveit_msgs::msg::PlanningScene scene;
+    scene.world.collision_objects.push_back(co);
+    scene.is_diff = true;
+    planning_scene_pub_->publish(scene);
 }
 
-void StateMachine::moveToTable(int block_index, int layer_index) {
-    
-}
-
-void StateMachine::placeBlock() {
-    
-}
+// stubs…
+void StateMachine::pickBlock() {}
+void StateMachine::moveToTable(int, int) {}
+void StateMachine::placeBlock() {}
